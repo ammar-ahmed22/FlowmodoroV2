17c58c442202155f11da5556a80a6ab4de0fd56c
ammar-ahmed22 <ammar.ahmed2203@gmail.com>
2021-11-18 05:50:55 -0500

    UI updates

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import "../css/Timer.css";
+import { secondsToHMS } from '../utils/time';
 
 const Timer = ({ elapsed, isBreak, workTime }) => {
 
-    const secondsToHMS = (secs) =>{
-        const h = Math.floor(secs / 3600);
-        const m = Math.floor(secs % 3600 / 60);
-        const s = Math.floor(secs % 3600 % 60);
-
-        return {
-            hours: h < 10 ? `0${h}` : h,
-            minutes: m < 10 ? `0${m}` : m,
-            seconds: s < 10 ? `0${s}` : s
-        };
-    }
+    
     
     const { hours, minutes, seconds } = secondsToHMS(isBreak ? (workTime - elapsed) : elapsed);
     return (
-        <div className="timer d-flex justify-content-center">
+        <div className="timer d-flex justify-content-between">
             <div className="timer-value d-flex flex-column align-items-center">
                 <span className="text-light fw-light">Hours</span>
                 <span className="text-light fw-bold display-1">{hours}</span>
